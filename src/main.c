efca8bee6024b1ac8317ff9e435f2b36b6807db7
ErikaKoro <erikakoro.03@gmail.com>
2023-12-23 02:21:03 +0200

    All timers test

--- src/main.c
+++ src/main.c
@@ -21,54 +21,54 @@ int main(int argc, char const *argv[]){
     queue q;
     queueInit(&q, QUEUE_SIZE, NUMBER_OF_THREADS);
 
-    // Timer t1;
-    // timerInit(&t1, &q, PERIOD1, (RUN_TIME * 1000) / PERIOD1, STARTDELAY);
+    Timer t1;
+    timerInit(&t1, &q, PERIOD1, (RUN_TIME * 1000) / PERIOD1, STARTDELAY);
 
     Timer t2;
     timerInit(&t2, &q, PERIOD2, (RUN_TIME * 1000) / PERIOD2, STARTDELAY);
 
-    // Timer t3;
-    // timerInit(&t3, &q, PERIOD3, (RUN_TIME * 1000) / PERIOD3, STARTDELAY);
+    Timer t3;
+    timerInit(&t3, &q, PERIOD3, (RUN_TIME * 1000) / PERIOD3, STARTDELAY);
 
     threadConsumers cons;
     threadsInit(&cons, &q, NUMBER_OF_THREADS);
     
-    // t1.timerFnc(&t1);
+    t1.timerFnc(&t1);
     t2.timerFnc(&t2);
-    // t3.timerFnc(&t3);
+    t3.timerFnc(&t3);
 
-    // start(&t1);
+    start(&t1);
     start(&t2);
-    // start(&t3);
+    start(&t3);
     // startat(&t, 36, 15, 20, 20, 12, 2023);
 
     joinThreads(&cons);
 
     FILE *fp;
-    fp = fopen("execution_times/producer_100ms.txt", "w");
+    fp = fopen("execution_times/producer_all.txt", "w");
     fprintf(fp, "%d\n%d\n", NUMBER_OF_TIMERS, RUN_TIME);
-    // fprintf(fp, "%d\n%d\n", PERIOD1, t1.driftCounter);
+    fprintf(fp, "%d\n%d\n", PERIOD1, t1.driftCounter);
 
-    // for(int i = 0; i < (RUN_TIME * 1000) / PERIOD1; i++){
-    //     fprintf(fp, "%ld\n", t1.producerTimers[i]);
-    // }
+    for(int i = 0; i < (RUN_TIME * 1000) / PERIOD1; i++){
+        fprintf(fp, "%ld\n", t1.producerTimers[i]);
+    }
 
     fprintf(fp, "\n%d\n%d\n", PERIOD2, t2.driftCounter);
     for(int i = 0; i < (RUN_TIME * 1000) / PERIOD2; i++){
         fprintf(fp, "%ld\n", t2.producerTimers[i]);
     }
 
-    // fprintf(fp, "\n%d\n%d\n", PERIOD3, t3.driftCounter);
-    // for(int i = 0; i < (RUN_TIME * 1000) / PERIOD3; i++){
-    //     fprintf(fp, "%ld\n", t3.producerTimers[i]);
-    // }
+    fprintf(fp, "\n%d\n%d\n", PERIOD3, t3.driftCounter);
+    for(int i = 0; i < (RUN_TIME * 1000) / PERIOD3; i++){
+        fprintf(fp, "%ld\n", t3.producerTimers[i]);
+    }
 
     fclose(fp);
 
     // stop timers
-    // t1.stopFnc(&t1);
+    t1.stopFnc(&t1);
     t2.stopFnc(&t2);
-    // t3.stopFnc(&t3);
+    t3.stopFnc(&t3);
     
     return 0;
 }
